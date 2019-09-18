33754d3557e647765426598d29e601d1b27c5e9b
chenzx <chenzx@suc.com>
2019-09-18 15:15:50 +0800

    创建项目

---
+++ easy-ud-core/src/main/java/util/fileUtil.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @Auther: "Chenzx"
+ * @Date: 2019/9/18 14:30
+ * @Description:
+ */
+public class fileUtil {
+    public static String getMd5(File file) {
+        return "";
+    }
+}

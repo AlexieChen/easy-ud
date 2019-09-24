245ffb845fb0afaa719e2c2d8e73761e0350d63a
aosm <aosm@sucsoft.com>
2019-09-24 16:22:03 +0800

    easy-ud-sql

--- easy-ud-sql/src/main/java/com/sucsoft/easyudsql/service/StorageFileInfo.java
+++ easy-ud-sql/src/main/java/com/sucsoft/easyudsql/service/StorageFileInfo.java
@@ -1,13 +1,10 @@
 package com.sucsoft.easyudsql.service;
 
 import com.sucsoft.easyudsql.bean.FileInfo;
-import com.sucsoft.easyudsql.bean.Fileform;
 import com.sucsoft.easyudsql.repository.FileInfoMapper;
-import com.sucsoft.jt.acjtutil.JtIdCreateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -21,22 +18,37 @@ public class StorageFileInfo {
     @Autowired
     private FileInfoMapper fileInfoMapper;
 
+    /**
+     * @Description 所有文件信息
+     */
     private List<FileInfo> queryAllFileInfo() {
         return fileInfoMapper.selectAll();
     }
 
+    /**
+     * @Description 通过Id查找对应文件信息
+     */
     private FileInfo queryFileInfoById(String id) {
         return fileInfoMapper.selectById(id);
     }
 
+    /**
+     * @Description 通过md5值查找对应文件信息
+     */
     private FileInfo queryFileInfoByMd5(String md5) {
         return fileInfoMapper.selectByMd5(md5);
     }
 
+    /**
+     * @Description 保存文件信息
+     */
     private void saveFileInfo(FileInfo fileInfo) {
         fileInfoMapper.save(fileInfo);
     }
 
+    /**
+     * @Description 更新文件信息
+     */
     private void updateFileInfo(FileInfo fileInfo) {
         fileInfoMapper.update(fileInfo);
     }

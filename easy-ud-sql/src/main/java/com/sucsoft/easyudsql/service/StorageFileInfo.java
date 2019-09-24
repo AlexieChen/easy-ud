52960919a19edc8fc382ce749c538174c898065b
aosm <aosm@sucsoft.com>
2019-09-24 16:23:00 +0800

    easy-ud-sql

--- easy-ud-sql/src/main/java/com/sucsoft/easyudsql/service/StorageFileInfo.java
+++ easy-ud-sql/src/main/java/com/sucsoft/easyudsql/service/StorageFileInfo.java
@@ -19,35 +19,35 @@ public class StorageFileInfo {
     private FileInfoMapper fileInfoMapper;
 
     /**
-     * @Description 所有文件信息
+     * @Note 所有文件信息
      */
     private List<FileInfo> queryAllFileInfo() {
         return fileInfoMapper.selectAll();
     }
 
     /**
-     * @Description 通过Id查找对应文件信息
+     * @Note 通过Id查找对应文件信息
      */
     private FileInfo queryFileInfoById(String id) {
         return fileInfoMapper.selectById(id);
     }
 
     /**
-     * @Description 通过md5值查找对应文件信息
+     * @Note 通过md5值查找对应文件信息
      */
     private FileInfo queryFileInfoByMd5(String md5) {
         return fileInfoMapper.selectByMd5(md5);
     }
 
     /**
-     * @Description 保存文件信息
+     * @Note 保存文件信息
      */
     private void saveFileInfo(FileInfo fileInfo) {
         fileInfoMapper.save(fileInfo);
     }
 
     /**
-     * @Description 更新文件信息
+     * @Note 更新文件信息
      */
     private void updateFileInfo(FileInfo fileInfo) {
         fileInfoMapper.update(fileInfo);

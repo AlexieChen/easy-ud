65795ead390a3cf91c8a0bcc5c018b8c834c954c
chenzx <chenzx@suc.com>
2019-12-11 17:08:17 +0800

    配置mongo

--- easy-ud-core/src/main/java/com/sucsoft/easyudcore/bean/FileUploadStatus.java
+++ easy-ud-core/src/main/java/com/sucsoft/easyudcore/bean/FileUploadStatus.java
@@ -4,9 +4,10 @@ package com.sucsoft.easyudcore.bean;
  * @author : Chenx
  * @date : 2019/9/20 10:13
  */
+/*
 public class FileUploadStatus {
 
     public static final int FILE_UPLOAD_STATUS_SUC = 0;
 
     public static final int FILE_UPLOAD_STATUS_FAIL = 1;
-}
+}*/
